c7640f8ac80811ac551a7736c6564534e44e3908
fatimasqlbi <117467543+fatimasqlbi@users.noreply.github.com>
2022-12-21 15:14:17 -0600

    Update streamlit_app.py
    #Allow end user to add a fruit to list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,15 @@ def get_fruit_load_list():
 If streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()    
    streamlit.dataframe(my_data_rows)
    streamlit.stop()
    
 #Allow end user to add a fruit to list. 
 def insert_row_snowflake(new_fruit):
    with my_cnx.corsor() as my_cur:
+       my_cur.execute("insert into fruit_load_list values('" + ???? +"')')
+                      return "Thanks for adding "+new_fruit
       
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streanlit.button('Add a Fruit to the List'):
