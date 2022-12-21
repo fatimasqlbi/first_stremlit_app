69795acc35bb71b59c9398f7b92cd209c87f53cc
fatimasqlbi <117467543+fatimasqlbi@users.noreply.github.com>
2022-12-21 14:27:51 -0600

    Update streamlit_app.py
    organized code by adding try except statement to first output.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,24 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call.
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+        
 streamlit.write('The user entered ', fruit_choice)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#delete this later 'streamlit.text(fruityvice_response.json())'# just writes the data to the screen,
-
-# take json version of the response and normalize it. 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#output the secreen as a table.
-streamlit.dataframe(fruityvice_normalized)
-
-#Add a STOP Command to Focus Our Attention
 streamlit.stop()
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
